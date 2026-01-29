87baf5b8efc2eaa63c7124d9dd54921a18462288
Athov07 <atharvadsare17@gmail.com>
2026-01-29 15:37:11 +0530

    Initial project and packeges setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <div className="h-screen bg-purple-600 flex items-center justify-center">
+      <h1 className="text-6xl font-black text-yellow-300">
+        FINAL TEST OK
+      </h1>
+    </div>
+  )
+}
+
+export default App
